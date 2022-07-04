f8d0c21b1788f758decc03f1a75bdfd7ebaf976b
BigTree777 <82656982+BigTree777@users.noreply.github.com>
2022-07-04 11:53:48 +0900

    Update: 2022/07/04

--- submissions/abc257/abc257_c.cpp
+++ submissions/abc257/abc257_c.cpp
@@ -3,85 +3,46 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 
-ull func(vector<ull>& vecChild, vector<ull>& vecAdult){
-    ull max_child = *max_element(vecChild.begin(), vecChild.end());
-    ull min_adult = *min_element(vecAdult.begin(), vecAdult.end());
-    ull ans = 0;
-    if(min_adult > max_child){
-        ans = vecAdult.size() + vecChild.size();
-        vecAdult.clear(); vecChild.clear();
-        return ans;
-    }
-    else if(min_adult == max_child){
-        ull a = 0;
-        ull c = 0;
-        for(ull i = 0; i < (ull)vecAdult.size(); i++){
-            if(vecAdult[i] == min_adult) a++;
-        }
-        for(ull i = 0; i < (ull)vecChild.size(); i++){
-            if(vecChild[i] == max_child) c++;
-        }
-        ans = (vecAdult.size() + vecChild.size() - (a + c)) + max(a, c);
-        vecAdult.clear(); vecChild.clear();
-        return ans;
-    }
-    else{
-        vector<ull> ex_vecChild;
-        vector<ull> ex_vecAdult;
-        ull count_max_child = 0;
-        ull count_min_adult = 0;
-        for(ull i = 0; i < (ull)vecAdult.size(); i++){
-            if((vecAdult[i] > min_adult) && (vecAdult[i] < max_child)) ex_vecAdult.push_back(vecAdult[i]);
-            else if(vecAdult[i] == min_adult) count_min_adult++;
-        }
-        for(ull i = 0; i < (ull)vecChild.size(); i++){
-            if((vecChild[i] > min_adult) && (vecChild[i] < max_child)) ex_vecChild.push_back(vecChild[i]);
-            else if(vecChild[i] == max_child) count_max_child++;
-        }
-        if(ex_vecAdult.size() == 0 || ex_vecChild.size() == 0){
-            ans = max(vecAdult.size(), vecChild.size());
-            vecAdult.clear(); vecChild.clear();
-            return ans;
-        }
-        ans = vecAdult.size() + vecChild.size() - ex_vecAdult.size() - ex_vecChild.size();
-        vecAdult.clear(); vecChild.clear();
-        vecAdult.reserve(ex_vecAdult.size()); vecChild.reserve(ex_vecChild.size());
-        vecAdult = ex_vecAdult; vecChild = ex_vecChild;
-        return ans;
-    }
-}
-
 int main(){
     ull N;
     cin >> N;
     string S;
     cin >> S;
 
-    ull W[N];
+    vector<ull> W(N);
     for(ull i = 0; i < N; i++) cin >> W[i];
-
-    vector<ull> vecChild;
-    vector<ull> vecAdult;
+    
+    vector<ll> numCorrect(N+1, 0);
+    vector<pair<ull, bool>> target;
     for(ull i = 0; i < N; i++){
-        if(S[i] == '0'){
-            vecChild.push_back(W[i]);
-        }
-        else{
-            vecAdult.push_back(W[i]);
-        }
-    }
-    if(vecChild.size() == 0 || vecAdult.size() == 0){
-        cout << N << endl;
-        return 0;
+        bool isAdult = true;
+        if(S[i] == '0') isAdult = false;
+        else numCorrect[0]++;
+
+        pair<ull, bool> p = make_pair(W[i], isAdult);
+        target.push_back(p);
     }
 
-    ull ans = 0;
-    while(1){
-        ans += func(vecChild, vecAdult);
-        if(vecAdult.size() == 0 || vecChild.size() == 0){
-            cout << ans << endl;
-            break;
+    sort(target.begin(), target.end());
+
+    ll num_same_weight = 0;
+    for(ull i = 0; i < N; i++){
+        if(target[i].second) num_same_weight--;
+        else num_same_weight++;
+
+        if(i != N-1){
+            if(target[i+1].first == target[i].first){
+                numCorrect[i+1] = numCorrect[i];
+                continue;
+            }
         }
+
+        numCorrect[i+1] = numCorrect[i] + num_same_weight;
+        num_same_weight = 0;
     }
+
+    ull ans = *max_element(numCorrect.begin(), numCorrect.end());
+    cout << ans << endl;
+
     return 0;
 }
